0bbd3833a50a12f3804e1f6d34b8141e898c49c4
AlexKosha <ovelikoshich@gmail.com>
2023-09-15 22:55:57 +0200

    save2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe, {
+  id: 'vimeo-player',
+});
+
+function savePlaybackTime() {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}
+
+function restorePlaybackTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+  }
+}
+
+player.on('timeupdate', throttle(savePlaybackTime, 1000));
+
+restorePlaybackTime();
